a0839b14760b06c2f999eadf500ab4b04da95e17
ClOU <szlouolsz@gmail.com>
2024-07-25 12:11:49 +0900

    경로 및 임포트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,14 @@ import JoinPage from "./pages/JoinPage/JoinPage";
 import JoinCompletePage from "./pages/JoinPage/JoinCompletePage"; // 추가된 부분
 import Match from "./pages/MatchingPage/matchingPage";
 import Market from "./pages/MarketPage/MarketPage";
-import Board from "./pages/BoardPage/BoardPage";
+import FoodBoard from "./pages/BoardPage/FoodBoard";
+import GeneralBoard from "./pages/BoardPage/GeneralBoard";
+import TipBoard from "./pages/BoardPage/TipBoard";
+
 import MatchingDetail from "./pages/MatchingPage/matchingDetail";
 // import MyPage from "./pages/MyPage/MyPage";
 import Header from "./components/Header/Header";
+import PostWriter from "./pages/BoardPage/PostWriter";
 
 const App: React.FC = () => {
   return (
@@ -20,7 +24,10 @@ const App: React.FC = () => {
           <Route path="/matching" element={<Match />} />
           <Route path="/matching/detail" element={<MatchingDetail />} />
           <Route path="/market" element={<Market />} />
-          <Route path="/board/general" element={<Board />} />
+          <Route path="/board/general" element={<GeneralBoard />} />
+          <Route path="/board/food" element={<FoodBoard />} />
+          <Route path="/postform" element={<PostWriter />} />
+          <Route path="/board/tips" element={<TipBoard />} />
           {/* <Route path="/mypage" element={<MyPage />} /> */}
           <Route path="/loginpage" element={<LoginPage />} />
           <Route path="/joinpage" element={<JoinPage />} />
