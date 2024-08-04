8b0c5554d7f9c1a75d14fceab9827e4872c89396
yoglee64 <l3060921@naver.com>
2024-08-04 23:36:54 +0900

    update react router system

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,44 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import matchingRoutes from './routes/MatchingRoutes';
+import boardRoutes from './routes/BoardRoutes';
+import Mainpage from "./pages/mainpage/mainpage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // App 컴포넌트를 루트로 설정
+    children: [
+      { path: "/", element: <Mainpage /> },
+      ...matchingRoutes,
+      ...boardRoutes
+    ]
+  }
+]);
+
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
 reportWebVitals();
